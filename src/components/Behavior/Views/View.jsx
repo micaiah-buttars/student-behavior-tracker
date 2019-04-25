aa07b579d98ee64ff9a04dc2775406bba0662d01
Micaiah Buttars <michael.buttars@gmail.com>
2019-04-25 14:57:17 -0600

    computer might crash commit

--- src/components/Behavior/Views/View.jsx
+++ src/components/Behavior/Views/View.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {requestStudent} from '../../../ducks/studentReducer'
 import {requestLogs} from '../../../ducks/recordReducer'
+import {Doughnut} from 'react-chartjs-2'
+import { arrayExpression } from '@babel/types';
 
 class View extends Component{
     constructor(props){
@@ -17,8 +19,55 @@ class View extends Component{
 
     render(){
         console.log(this.props)
+        let logs = this.props.logs || ['']
+        // const onTask = [logs].filter(l => l.behavior_type_id === 1)
+
+        console.log(logs)
+        let onTask = 0
+        let discouraged = 0
+        let replacement = 0
+        let noEntry = 0
+        function tally(arr){
+            for(let i = 0; i < arr.length; i++){
+                console.log(arr[i])
+            }
+
+        }
+
+        tally(logs)
+        console.log(onTask)
+
+        
+        const data = {
+            labels: [
+                'Red',
+                'Green',
+                'Yellow'
+            ],
+            datasets: [{
+                data: [300, 50, 100],
+                backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+                ],
+                hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+                ]
+            }]
+        };
     return (
+        <div>
         <div>VIEW</div>
+        <Doughnut data={data}/>
+
+
+
+
+        </div>
+
 
     )
     }
